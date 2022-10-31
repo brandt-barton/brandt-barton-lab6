7c02fb753415c300b2b386cc0368116e0456b50f
brandt-barton <b.barton@ufl.edu>
2022-10-31 19:19:04 -0400

    Initial commit to add two integers.

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Name: Brandt Barton
+Class: COP3502C Fall 2022
+Proj: Lab6
+Date: 10/31/2022
+
+"""
+
+
+def main():
+
+    num_1 = int(input('Input any whole number to be added: '))
+    num_2 = int(input('Input another number: '))
+
+    sum = num_1 + num_2
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
+
+
